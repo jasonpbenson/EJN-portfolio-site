a66d2101d736dada649fd52a7f33645c731b1f26
jasonpbenson <jasonbenson3rd@gmail.com>
2019-10-22 15:10:26 -0400

    troubleshooting build error; removed gatsby-image component

--- src/components/LandingImage.js
+++ src/components/LandingImage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Img from "gatsby-image"
+// import Img from "gatsby-image"
 import styled from "styled-components"
 import flowerSvg from "../assets/images/svg-icons/flower-with-four-petals.svg"
 
@@ -11,12 +11,12 @@ const LandingImage = props => {
     min-width: 425px;
     position: relative;
     width: 35%;
-  `
-  const Svg = styled.img`
-    left: 0;
-    position: absolute;
-    top: -1%;
-    width: 100%;
+    .flower-shape {
+      left: 0;
+      position: absolute;
+      top: -1%;
+      width: 100%;
+    }
   `
 
   const DisplayRandomLandingImage = () => {
@@ -24,13 +24,18 @@ const LandingImage = props => {
     if (imageData.edges.length > 0) {
       let index = Math.floor(Math.random() * Math.floor(imageData.edges.length))
       return (
-        <Img
-          fluid={imageData.edges[index].node.childImageSharp.fluid}
+        <img
           src={imageData.edges[index].node.childImageSharp.fluid.src}
-          placeholderStyle={{ display: "none" }}
           alt="artwork detail"
-          imgStyle={{ zIndex: "100" }}
+          style={{ zIndex: "100", position: "relative" }}
         />
+        // <Img
+        //   fluid={imageData.edges[index].node.childImageSharp.fluid}
+        //   src={imageData.edges[index].node.childImageSharp.fluid.src}
+        //   placeholderStyle={{ display: "none" }}
+        //   alt="artwork detail"
+        //   imgStyle={{ zIndex: "100" }}
+        // />
       )
     } else {
       return <h1>ERIN JANE NELSON</h1>
@@ -40,7 +45,8 @@ const LandingImage = props => {
   return (
     <ImageContainer>
       <DisplayRandomLandingImage />
-      <Svg
+      <img
+        className="flower-shape"
         src={flowerSvg}
         alt="flower shape placed behind artwork detail cutout to create greater contrast with background"
       />
